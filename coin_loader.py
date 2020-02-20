1bdc7e7686d3729159e1181307440e4880a8fbbf
maccuryj <jean.maccury@gmail.com>
2020-02-20 18:13:12 +0100

    Coin loader documentation

--- coin_loader.py
+++ coin_loader.py
@@ -8,13 +8,50 @@ import sklearn
 from sklearn.model_selection import train_test_split
 from skimage import io
 
+"""
+### EXAMPLE CODE EXECUTION TO CREATE COIN DATALOADER ###
+
+
+# Flag that indicates whether a cuda capable GPU is available
+# Set the device to cuda if a GPU is available
+is_gpu = torch.cuda.is_available()
+device = torch.device("cuda" if is_gpu else "cpu")
+
+dataset = AncientCoins("Coinset", is_gpu)
+num_classes = len(dataset.get_labels())
+"""
 
 class AncientCoins():
-    def __init__(self, root_dir, is_gpu, batch_size=32, workers=2, test_size=0.25):
+    """
+    Dataset creation class, providing on instantiation a stratified train/test split.
+    Provides coin specific datasets and dataloaders for easy integration with PyTorch.
+
+    Attributes:
+        root_dir (str):             Root directory holding the image data
+        labels (Dict):              Mapping dictionary for labels (if original labels are not (0...N))
+        trainset (CoinDataset):     Training coin set
+        valset (CoinDataset):       Validation coin set
+        trainloader (DataLoader):   Training coin data loader
+        valloader (DataLoader):     Validation coin data loader
+    """
+    def __init__(self, root_dir, img_size, is_gpu, batch_size=32, shuffle=True, workers=2, test_size=0.25, transform=None):
+        """
+        Instantation of datasets and dataloaders
+
+        Args:
+            img_size (int):                 Final width and height of images
+            is_gpu (str):                   Description of CUDA device
+            batch_size (int):               Size of mini-batches delivered by data loader
+            shuffle (boolean):              Shuffle indices of data points
+            workers (int):                  Parallelisation factor
+            test_size (float):              Fraction of data used for validation
+            transform (list(transforms)):   List of image transformations
+        """
         labels = {}
         X = []
         y = []                
         
+        #Pull the image classes and filenames from the data folder 
         for i, d in enumerate(os.listdir(root_dir)):
             labels[i] = int(d)
             for f in os.listdir(os.path.join(root_dir, str(d))):       
@@ -24,34 +61,47 @@ class AncientCoins():
         self.root_dir = root_dir     
         self.labels = labels
 
-        #Stratified train test split
+        # Stratified train test split
         self.train_X, self.test_X, self.train_y, self.test_y = train_test_split(X, y, test_size=test_size, stratify=y)
-        #Create training and validation set as defined in CoinsDataset
-        self.trainset, self.valset = self.get_datasets()
-        #Create PyTorch Dataloaders for coin data
-        self.trainloader = self.get_dataloaders(self.trainset, batch_size, workers, is_gpu)
-        self.valloader = self.get_dataloaders(self.valset, batch_size, workers, is_gpu)
+
+        # Create training and validation set as defined in CoinsDataset
+        if transform is None:
+            transform = [
+            transforms.ToPILImage(),
+            transforms.Resize((img_size,img_size)),
+            #transforms.RandomHorizontalFlip(),
+            transforms.Grayscale(num_output_channels=1),
+            transforms.ToTensor()]
+
+        self.trainset = CoinsDataset(self.root_dir, self.train_X, self.train_y, self.labels, img_size, transform)
+        self.valset = CoinsDataset(self.root_dir, self.test_X, self.test_y, self.labels, img_size, transform)        
+        # Create PyTorch Dataloaders for coin data
+        self.trainloader = DataLoader(self.trainset, batch_size=batch_size, shuffle=shuffle, num_workers=workers, pin_memory=is_gpu)
+        self.valloader = DataLoader(self.valset, batch_size=batch_size, shuffle=shuffle, num_workers=workers, pin_memory=is_gpu)
         
-    def check_exists():
+    def check_exists(self):
+        """
+        Return whether the data directory exists
+        """
         return self.root_dir in os.listdir()
-
-    def get_datasets(self):
-        trainset = CoinsDataset(self.root_dir, self.train_X, self.train_y, self.labels)
-        valset = CoinsDataset(self.root_dir, self.test_X, self.test_y, self.labels)
-        
-        return trainset, valset
-
-    def get_dataloaders(self, dataset, batch_size, workers, is_gpu):
-        loader = DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=workers, pin_memory=is_gpu)
-        return loader
         
 
-class CoinsDataset(Dataset):  
-    def __init__(self, root_dir, X, y, labels):
+class CoinsDataset(Dataset):
+    """
+    Coin Dataset class which handles the provision of tensors
+    on the basis of images in the data folder structure
+
+    Attributes:
+        X (list(str)):          Filenames of coin images
+        y (list(int)):          Class labels of coin images
+    """
+    def __init__(self, root_dir, X, y, labels, img_size, transform):
         self.X = X
         self.y = y
         self.root_dir = root_dir
         self.labels = labels
+        self.img_size
+        self.transform = transform
         
     def __len__(self):
         return len(self.X)
@@ -63,12 +113,7 @@ class CoinsDataset(Dataset):
             print(os.path.join(self.root_dir, str(self.labels[self.y[idx]]), self.X[idx]))
             print(ValueError)
 
-        X = transforms.Compose([
-            transforms.ToPILImage(),
-            transforms.Resize((28,28)),
-            transforms.RandomHorizontalFlip(),
-            transforms.Grayscale(num_output_channels=1),
-            transforms.ToTensor()])(x)
+        X = transforms.Compose(self.transform)(x)
         y = self.y[idx]
 
         return X,y
